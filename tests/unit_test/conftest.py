85d7cc2b03d19b7d312db068cb525e4ea931b849
AatroXiss <antoine.beaudesson@gmail.com>
2022-02-01 10:31:39 +0100

    v.0.2.3 updates on conftests

--- tests/unit_test/conftest.py
+++ tests/unit_test/conftest.py
@@ -1,6 +1,12 @@
 import pytest
 
+# local application imports
 import server
+
+
+# constants
+PATH_COMPETITIONS_TESTS = "tests/resources/test_competitions.json"
+PATH_CLUBS_TESTS = "tests/resources/test_clubs.json"
 
 
 @pytest.fixture
@@ -9,3 +15,37 @@ def client():
     client = server.app.test_client()
 
     yield client
+
+
+@pytest.fixture
+def info_for_testing(mocker):
+    """
+    Declaration of all information to pass
+    tests with test data.
+    """
+
+    path_clubs_json = mocker.patch.object(
+        server,
+        'PATH_CLUBS',
+        PATH_CLUBS_TESTS
+    )
+    path_competitions_json = mocker.patch.object(
+        server,
+        'PATH_COMPETITIONS',
+        PATH_COMPETITIONS_TESTS
+    )
+
+    competitions = server.load_competitions()
+    clubs = server.load_clubs()
+
+    comp = mocker.patch.object(server, 'competitions', competitions)
+    c = mocker.patch.object(server, 'clubs', clubs)
+
+    data = {
+        'competitions': comp,
+        'clubs': c,
+        'PATH_CLUBS': path_clubs_json,
+        'PATH_COMPETITIONS': path_competitions_json
+    }
+
+    return data
